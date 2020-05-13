62345ea533adca7392f39a2e573df28d493a2b4e
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-05-12 20:00:53 -0600

    Variables de configuracion para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD3aMovZiRF9VNqaLTlFjJ7eyo0O2UHm0Q",
+    authDomain: "angularcrud-34f93.firebaseapp.com",
+    databaseURL: "https://angularcrud-34f93.firebaseio.com",
+    projectId: "angularcrud-34f93",
+    storageBucket: "angularcrud-34f93.appspot.com",
+    messagingSenderId: "260281209591",
+    appId: "1:260281209591:web:57ed1df2afe41da97079f2",
+    measurementId: "G-V7VFVTR8WD"
+  }
 };
